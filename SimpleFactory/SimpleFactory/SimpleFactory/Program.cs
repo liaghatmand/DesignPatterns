8cb150fc0d72b9c8980fb9cb65039d980441e3b3
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-02-15 19:59:02 +0330

    Add Feature SimpleFactory

---
+++ SimpleFactory/SimpleFactory/SimpleFactory/Program.cs
@@ -0,0 +1,10 @@
+﻿using SimpleFactory;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IVehicle vehicle = SimpleFactory.SimpleFactory.Create("car");
+
+        Console.WriteLine(vehicle.Kind());
+    }
+}
---
+++ SimpleFactory/SimpleFactory/SimpleFactory/SimpleFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SimpleFactory
+{
+    public class SimpleFactory
+    {
+        public static IVehicle Create(string type)
+        {
+            switch (type)
+            {
+                case "car":
+                    Car car = new();
+                    return car;
+                case "truck": 
+                   Truck truck = new();
+                    return truck;
+                default:
+                    throw new Exception("Wrong user type passed.");
+            }
+        }
+    }
+
+    public interface IVehicle {
+        public string Kind();
+    }
+    public class Car : IVehicle {
+        public string Kind()
+        {
+            return "you made a car!";
+        }
+    }
+    public class Truck : IVehicle {
+        public string Kind()
+        {
+            return "you made a truck!";
+        }
+    }
+}
+
