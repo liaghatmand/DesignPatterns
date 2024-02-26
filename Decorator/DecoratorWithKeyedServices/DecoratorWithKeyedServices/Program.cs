cf23df6c17c4fad1c5a6324144ebbf813559a533
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-02-27 00:09:06 +0330

    enhancement
    using decorator to give control to configuration

--- Decorator/DecoratorWithKeyedServices/DecoratorWithKeyedServices/Program.cs
+++ Decorator/DecoratorWithKeyedServices/DecoratorWithKeyedServices/Program.cs
@@ -7,9 +7,15 @@ using System;
 var services = new ServiceCollection()
     .AddKeyedSingleton<ISMSProvider,AsaSMSProvider >(AsaSMSProvider.ProviderKey)
     .AddKeyedSingleton<ISMSProvider,FaraSMSProvider>(FaraSMSProvider.ProviderKey)
-    .AddScoped<IClient, Client>()
-    .BuildServiceProvider() ;
-    
-var client = services.GetRequiredService<IClient>();
-client.Send();
+    .AddSingleton<ISMSProvider,SMSDecorator>()
+.AddSingleton<SMSDecorator>(services =>
+      {
+          var asa = services.GetRequiredKeyedService<ISMSProvider>(AsaSMSProvider.ProviderKey);
+          var fara = services.GetRequiredKeyedService<ISMSProvider>(FaraSMSProvider.ProviderKey);
+          //some conditional logic reading from config to choose fara or asa
+              return new SMSDecorator(asa);
+      }
+    ).BuildServiceProvider(); 
 
+var client = services.GetRequiredService<SMSDecorator>();
+client.Send();
