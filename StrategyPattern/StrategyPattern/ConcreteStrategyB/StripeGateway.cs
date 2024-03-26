6852611313d843b94d93ad1f1e81bc2f03a0f5b0
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-03-26 21:04:55 +0330

    Upload project on github

---
+++ StrategyPattern/StrategyPattern/ConcreteStrategyB/StripeGateway.cs
@@ -0,0 +1,12 @@
+﻿using StrategyPattern.IStrategy;
+
+namespace StrategyPattern.ConcreteStrategyB
+{
+    public class StripeGateway : IPaymentGateway
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Processing payment of {amount} via Stripe.");
+        }
+    }
+}
---
+++ StrategyPattern/StrategyPattern/Context/PaymentProcessor.cs
@@ -0,0 +1,25 @@
+﻿using StrategyPattern.IStrategy;
+
+namespace StrategyPattern.Context
+{
+    public class PaymentProcessor
+    {
+        private IPaymentGateway _paymentGateway;
+
+        public PaymentProcessor(IPaymentGateway paymentGateway)
+        {
+            _paymentGateway = paymentGateway;
+        }
+
+        public void SetPaymentGateway(IPaymentGateway paymentGateway)
+        {
+            _paymentGateway = paymentGateway;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            _paymentGateway.ProcessPayment(amount);
+        }
+
+    }
+}
