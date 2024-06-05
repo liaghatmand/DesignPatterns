ec7807a90eb1b438e2e85b5f8064c4a5b4614bb4
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-06-05 17:43:59 +0330

    Add NextMethod

--- ChainOfResponsibility/ChainOfResponsibility/ChainOfResponsibility/Handlers/Handler.cs
+++ ChainOfResponsibility/ChainOfResponsibility/ChainOfResponsibility/Handlers/Handler.cs
@@ -2,5 +2,12 @@
 {
     public abstract class Handler
     {
+        protected Handler _nextHandler;
+
+        public void SetNext(Handler nextHandler)
+        {
+            _nextHandler = nextHandler;
+        }
+
     }
 }
