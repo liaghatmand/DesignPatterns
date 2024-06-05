f00dc9d9380e9ca4ccf545d95a6bc7d2abdd130d
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-06-05 18:24:58 +0330

    Add Program Class

---
+++ ChainOfResponsibility/ChainOfResponsibility/ChainOfResponsibility/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using ChainOfResponsibility.ConcreteHandler1;
+using ChainOfResponsibility.ConcreteHandler2;
+using ChainOfResponsibility.ConcreteHandler3;
+using ChainOfResponsibility.Handlers;
+
+namespace ChainOfResponsibility.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Handler authHandler = new AuthenticationHandler();
+            Handler authorizationHandler = new AuthorizationHandler();
+            Handler adminHandler = new AdminHandler();
+
+            authHandler.SetNext(authorizationHandler);
+            authorizationHandler.SetNext(adminHandler);
+
+            Console.WriteLine("Sending 'Authenticated' request:");
+            authHandler.HandleRequest("Authenticated");
+
+            Console.WriteLine("\nSending 'Authorized' request:");
+            authHandler.HandleRequest("Authorized");
+
+            Console.WriteLine("\nSending 'Admin' request:");
+            authHandler.HandleRequest("Admin");
+
+            Console.WriteLine("\nSending 'Unknown' request:");
+            authHandler.HandleRequest("Unknown");
+        }
+
+    }
+}
