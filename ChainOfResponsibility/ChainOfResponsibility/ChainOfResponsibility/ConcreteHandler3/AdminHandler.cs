5e46ec6b97b7e24cfb95c26705fcca72d05cc78a
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-06-05 18:20:12 +0330

    Add AdminHandler Class

---
+++ ChainOfResponsibility/ChainOfResponsibility/ChainOfResponsibility/ConcreteHandler3/AdminHandler.cs
@@ -0,0 +1,17 @@
+﻿using ChainOfResponsibility.Handlers;
+
+namespace ChainOfResponsibility.ConcreteHandler3
+{
+    public class AdminHandler : Handler
+    {
+        protected override bool ProcessRequest(string request)
+        {
+            if (request == "Admin")
+            {
+                Console.WriteLine("User is admin.");
+                return true;
+            }
+            return false;
+        }
+    }
+}
